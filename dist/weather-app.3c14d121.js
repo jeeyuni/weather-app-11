d88d04fe4f121aacccb235b0ef42766437d5a965
Jiyun Lee <kuri12071112@gmail.com>
2025-04-11 17:40:26 +0700

    Current weather container added

--- dist/weather-app.3c14d121.js
+++ dist/weather-app.3c14d121.js
@@ -669,6 +669,38 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"kTBnD":[function(require,module,exports,__globalThis) {
 require("88936c056fc44dd2").config();
 console.log("8dbc229683d9e2c9e0217ddebe650687");
+async function fetchWeather() {
+    const apiKey = "8dbc229683d9e2c9e0217ddebe650687";
+    const city = 'Seoul';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    //getting dta from the API
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        const data = await response.json();
+        displayCurrentWeather(data);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+}
+function displayCurrentWeather(data) {
+    const newImageElement = document.createElement('img');
+    newImageElement.src = ` https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const weather = document.getElementById("weather");
+    const weatherTemp = document.getElementById("weather-temp");
+    const weatherHumidity = document.getElementById("weather-humidity");
+    const weatherDescription = document.getElementById("weather-description");
+    //clearing previous contents
+    weather.innerHTML = '';
+    weatherTemp.innerHTML = '';
+    weatherHumidity.innerHTML = '';
+    weatherDescription.innerHTML = '';
+    weather.appendChild(newImageElement);
+    weatherTemp.textContent = `${data.main.temp}\u00B0F`;
+    weatherHumidity.textContent = `${data.main.humidity}% Humidity`;
+    weatherDescription.textContent = `${data.weather[0].description}`;
+}
+fetchWeather();
 
 },{"88936c056fc44dd2":"5TE9A"}],"5TE9A":[function(require,module,exports,__globalThis) {
 var process = require("a81606b34c5d81aa");
--- script.js
+++ script.js
@@ -1,3 +1,47 @@
 require('dotenv').config();
-
 console.log(process.env.API_KEY);
+
+async function fetchWeather() {
+    const apiKey = process.env.API_KEY;
+    const city = 'Seoul';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    
+    //getting dta from the API
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        displayCurrentWeather(data);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+
+
+}
+
+function displayCurrentWeather(data) {
+    const newImageElement = document.createElement('img');
+    newImageElement.src = ` https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+    const weather = document.getElementById("weather");
+    const weatherTemp = document.getElementById("weather-temp");
+    const weatherHumidity = document.getElementById("weather-humidity");
+    const weatherDescription = document.getElementById("weather-description");
+
+    //clearing previous contents
+    weather.innerHTML = '';
+    weatherTemp.innerHTML = '';
+    weatherHumidity.innerHTML = '';
+    weatherDescription.innerHTML = '';
+
+    weather.appendChild(newImageElement);
+    weatherTemp.textContent = `${data.main.temp}\u00B0F`;
+    weatherHumidity.textContent = `${data.main.humidity}% Humidity`;
+    weatherDescription.textContent = `${data.weather[0].description}`;
+
+}
+
+fetchWeather();
+
