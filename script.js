ebf4acbcf240ceeec82b19ec2e235fc1f40ef5d0
Jiyun Lee <kuri12071112@gmail.com>
2025-04-11 15:44:46 +0700

    Testing API

---
+++ script.js
@@ -0,0 +1,36 @@
+async function fetchWeather() {
+    let city = "seoul";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    displayCurrentWeather(data);
+}
+
+//placeholder function to display data on site for testing
+function displayCurrentWeather(data) {
+    const newImageElement = document.createElement('img');
+    newImageElement.src = ` https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+    const weather = document.getElementById("weather");
+    const weatherTemp = document.getElementById("weather-temp");
+    const weatherHumidity = document.getElementById("weather-humidity");
+    const weatherDescription = document.getElementById("weather-description");
+
+    //clearing pervious contents
+    weather.innerHTML = '';
+    weatherTemp.innerHTML = '';
+    weatherHumidity.innerHTML = '';
+    weatherDescription.innerHTML = '';
+
+    weather.appendChild(newImageElement);
+    weatherTemp.textContent = `${data.main.temp}\u00B0F`;
+    weatherHumidity.textContent = `${data.main.humidity}% Humidity`;
+    weatherDescription.textContent = `${data.weather[0].description}`;
+
+
+}
+
+fetchWeather();
